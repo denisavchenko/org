1e20d95f0727d46fd94cf74b8022b1cddc72f304
denisavchenko <denis33789@gmail.com>
2025-12-12 19:41:20 +0300

    basemap

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ const MapManager = {
             maxBoundsViscosity: 1.0 // Полностью ограничивает перемещение за границы
         }).setView(Config.mapCenter, Config.mapZoom);
         
-        const Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
+        const Stadia_AlidadeSmooth = L.tileLayer('https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png', {
             minZoom: 5,
             maxZoom: 20,
             attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
